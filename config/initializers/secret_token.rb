6e1659a6804c5e5e886ae9ae86d6837eca6128df
Erich Welz <erichwelz@gmail.com>
2014-03-01 15:16:51 -0500

    creating rails w/ data controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Code2014::Application.config.secret_key_base = 'f504e03f2b49777c4b256864f4608bab6b48b23a42e14985a4a485103475514b413f2b6853c321cbdaa78535c1cbcac54ab5ae5b06517ecc412e57ed50f742c5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Code2014::Application.config.session_store :cookie_store, key: '_code2014_session'
